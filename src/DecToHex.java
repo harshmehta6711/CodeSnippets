b69571acd066cfd053d8e12d0d02a814d9f02a3c
harshmehta6711 <harsh.mehta@sjsu.edu>
2017-12-09 17:57:51 -0800

    Decimal to Hexadecimal Number for positive numbers

--- src/DecToHex.java
+++ src/DecToHex.java
@@ -1,7 +1,20 @@
-public class DecToHex {
+class DecToHex {
     public String toHex(int num) {
-        if (num == 0) return null;
+        StringBuilder hexString = new StringBuilder();
+        char[] hex = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+        if (num == 0) return "0";
+        while (num > 0) {
+            int rem = num % 16;
+            hexString.append(hex[rem]);
+            num = num / 16;
+        }
+        return hexString.reverse().toString();
+    }
 
-        return null;
+    public static void main(String[] args) {
+        DecToHex obj1 = new DecToHex();
+        String result = "";
+        result = obj1.toHex(36);
+        System.out.println(result);
     }
 }
